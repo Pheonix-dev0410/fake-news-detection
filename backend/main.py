8aba878737aefb0a227aaa8e236a953361387867
Pheonix-dev0410 <pranavgarg0410@gmail.com>
2025-03-23 23:39:26 +0530

    Update CORS settings for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,8 @@ app.add_middleware(
         "http://localhost:8000",
         "http://127.0.0.1:8000",
         "http://localhost:8080",
-        "http://127.0.0.1:8080"
+        "http://127.0.0.1:8080",
+        "https://salmon-pond-0fbd3ac00.azurestaticapps.net"
     ],
     allow_credentials=True,
     allow_methods=["*"],
